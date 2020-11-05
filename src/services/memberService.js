c75b8dd6457d56c9cf3f9f69f1d8ebfc4d4f542b
sabbir-anwar <protik26@gmail.com>
2020-11-05 18:23:41 +0600

    Select component created

---
+++ src/services/memberService.js
@@ -0,0 +1,53 @@
+export const getMemberType = () => ([
+  {
+    id: "building_owner",
+    title: "Building Owner",
+  },
+  {
+    id: "flat_owner",
+    title: "Flat Owner",
+  },
+  {
+    id: "organization",
+    title: "Organization",
+  },
+  {
+    id: "tenant",
+    title: "Tenant",
+  },
+])
+
+export const getBloodGroup = () => ([
+  {
+    id: "a_pos",
+    title: "A+",
+  },
+  {
+    id: "o_pos",
+    title: "O+",
+  },
+  {
+    id: "b_pos",
+    title: "B+",
+  },
+  {
+    id: "ab_pos",
+    title: "AB+",
+  },
+  {
+    id: "a_neg",
+    title: "A-",
+  },
+  {
+    id: "o_neg",
+    title: "O-",
+  },
+  {
+    id: "b_neg",
+    title: "B-",
+  },
+  {
+    id: "ab_neg",
+    title: "AB-",
+  },
+])
