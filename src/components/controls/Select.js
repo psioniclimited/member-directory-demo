23382bb0925148bfc2126fb41a1e49b985932368
sabbir-anwar <protik26@gmail.com>
2020-11-07 17:00:03 +0600

    Select validation

--- src/components/controls/Select.js
+++ src/components/controls/Select.js
@@ -1,5 +1,6 @@
 import {
   FormControl,
+  FormHelperText,
   InputLabel,
   MenuItem,
   Select as MuiSelect,
@@ -7,16 +8,24 @@ import {
 import React from "react";
 
 export default function Select(props) {
-  const { name, label, value, onChange, options } = props;
+  const { name, label, value, options, error = null, onChange } = props;
   return (
-    <FormControl fullWidth size="small" variant="outlined">
+    <FormControl
+      fullWidth
+      size="small"
+      variant="outlined"
+      {...(error && { error: true })}
+    >
       <InputLabel>{label}</InputLabel>
       <MuiSelect label={label} name={name} value={value} onChange={onChange}>
         <MenuItem value="">None</MenuItem>
         {options.map((item) => (
-          <MenuItem key={item.id} value={item.id}>{item.title}</MenuItem>
+          <MenuItem key={item.id} value={item.id}>
+            {item.title}
+          </MenuItem>
         ))}
       </MuiSelect>
+      {error && <FormHelperText>{error}</FormHelperText>}
     </FormControl>
   );
 }
