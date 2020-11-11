804ba258807875bfb2c5ecba1e8161dc3458e915
sabbir-anwar <protik26@gmail.com>
2020-11-11 17:57:28 +0600

    currency page axios added

--- src/pages/Currency.js
+++ src/pages/Currency.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Currency() {
-  return (
-    <div>
-      Currency Tracker
-    </div>
-  )
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  });
+
+  return <div>Currency Tracker</div>;
 }
